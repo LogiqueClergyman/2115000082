90cf92a422b6ab5d35633523b2a0d2080c25b0e4
LogiqueClergyman <adityavishwanathanverma@gmail.com>
2024-07-12 15:47:38 +0530

    do not know where to get the data from apart from the test servers

--- backend/index.js
+++ backend/index.js
@@ -45,44 +45,78 @@ function addId(products) {
   });
 }
 
-function getData(company, category) {
-  fetch(`${api}/companies/${company}/categories/${category}/products`)
+function getData(company, category, top, minPrice, maxPrice) {
+  fetch(
+    `${api}/companies/${company}/categories/${category}/products?top=${top}&minPrice=${minPrice}&maxPrice=${maxPrice}`
+  )
     .then((res) => res.json())
     .then((data) => {
-      products = data.slice(0, n);
+        console.log(data)
       addId(products);
       ascendingSortedProducts = [];
       descendingSortedProducts = [];
       lastUpdated = Date.now();
+      console.log(products);
     });
 }
 
+let lastRequestedCompany = "";
+let lastRequestedCategory = "";
 app.get(
   `/companies/:companyName/categories/:categoryName/products`,
   (req, res) => {
     console.log(req.query);
     console.log(req.params);
-    if (products.length === 0 || Date.now() - lastUpdated > 100000) {
-      getData(req.params.companyName, req.params.categoryName);
+    // if (products.length === 0 || Date.now() - lastUpdated > 100000) {
+    //   getData(req.params.companyName, req.params.categoryName);
+    // }
+    if (
+      products.length === 0 ||
+      lastRequestedCompany !== req.params.companyName ||
+      lastRequestedCategory !== req.params.categoryName
+    ) {
+      getData(
+        req.params.companyName,
+        req.params.categoryName,
+        1000,
+        0,
+        1000000
+      );
+      lastRequestedCompany = req.params.companyName;
+      lastRequestedCategory = req.params.categoryName;
     }
     if (req.query.sort && req.query.sort === "asc") {
       if (ascendingSortedProducts.length === 0) ascendingSort(products);
       filteredProducts = ascendingSortedProducts;
-    }
-    if (req.query.sort && req.query.sort === "desc") {
+    } else if (req.query.sort && req.query.sort === "desc") {
       if (descendingSortedProducts.length === 0) descendingSort(products);
       filteredProducts = descendingSortedProducts;
+    } else {
+      filteredProducts = products;
     }
-    if (req.query.category) {
-      filteredProducts = filterByCategory(products, req.query.category);
-    }
-    if (req.query.company) {
-      filteredProducts = filterByCompany(filteredProducts, req.query.company);
-    }
+    // if (req.query.category) {
+    //   filteredProducts = filterByCategory(products, req.query.category);
+    // }
+    // if (req.query.company) {
+    //   filteredProducts = filterByCompany(filteredProducts, req.query.company);
+    // }
     if (req.query.minPrice || req.query.maxPrice) {
       let min = req.query.minPrice ? req.query.minPrice : 0;
       let max = req.query.maxPrice ? req.query.maxPrice : 1000000;
       filteredProducts = filterByPrice(filteredProducts, min, max);
+    }
+    if (!req.query.top) {
+      req.query.top = 10;
+    }
+    if (req.query.top && req.query.top > 10) {
+      let pageSize = 10;
+      if (!req.query.page) {
+        req.query.page = 1;
+      }
+      let start = (req.query.page - 1) * pageSize;
+      let end = start + pageSize;
+      if (end > filteredProducts.length) end = filteredProducts.length;
+      filteredProducts = filteredProducts.slice(start, end);
     }
     res.send(filteredProducts);
   }
